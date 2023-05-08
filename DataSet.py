542ef884ac0e661c7b1128b8d70ee5babcdd0b89
Heechan <ckstmznf@naver.com>
2023-05-08 14:09:59 +0900

    파일 정리

--- DataSet.py
+++
@@ -1,9 +0,0 @@
-import glob
-
-fieldImageFile = glob.glob("data/0/*")
-
-for idx, fieldFile in enumerate(fieldImageFile):
-    field = fieldFile.split("\\")[-1]
-    imageFile = glob.glob(fieldFile + "/*")
-
-    print(f"{field} : {len(imageFile)}개")
